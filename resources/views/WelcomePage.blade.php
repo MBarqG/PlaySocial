fded083f71e6e2c30024ad9a8ab0bf5982293e2d
Mohammed barq <mohammed_m_2001@live.com>
2023-07-04 20:41:23 +0300

    mian wlcome page complited

--- resources/views/WelcomePage.blade.php
+++ resources/views/WelcomePage.blade.php
@@ -26,15 +26,18 @@
     .custom-rounded {
     border-radius: 20px;
     }
+    .card-gap {
+    margin-bottom: 20px;
+    }
     </style>  
 </head>
 <body>
 <nav class="navbar navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#"><img src="{{ asset('images/logo.png') }}" alt="PlaySocial" width="112" height="21"></a>
-            <a class="nav-link" href="#">subscriptions</a>
-            <a class="nav-link" href="#">Goals</a>
-            <a class="nav-link" href="#">About us</a>
+            <a class="nav-link" href="#Subscriptions">subscriptions</a>
+            <a class="nav-link" href="#Goals">Goals</a>
+            <a class="nav-link" href="#About us">About us</a>
             <span><div class="container">
             <button type="button" class="btn btn-danger">Log in</button>
             <button type="button" class="btn btn-sm btn-outline-danger">Sign up</button>
@@ -55,7 +58,7 @@
 <br>
 
   <div class="container-xl text-center">
-  <h2 style="color: #dc3545" st>Subscriptions</h2>
+  <h2 style="color: #dc3545" id="Subscriptions">Subscriptions</h2>
   <div class="row row-cols-1 row-cols-md-3 g-4">
     <div class="col">
       <div class="card text-center custom-rounded h-100">
@@ -101,6 +104,104 @@
     </div>
   </div>
   </div>
+
+
+  <div class="container-xl text-center">
+    <h2 style="color: #dc3545" id="Goals">Goals</h2>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="5000">
+          <img src="{{ asset('images/share slide.png') }}" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Content Sharing</h5>
+            <p>providing a medium for users to share their videos and other forms of content with a wide audience.</p>
+          </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+          <img src="{{ asset('images\Monetization slide.png') }}" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Monetization and Revenue Generation</h5>
+            <p>offer opportunities for content creators to monetize their work through advertising, sponsorships, subscriptions, or crowdfunding.</p>
+          </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+          <img src="{{ asset('images\Collaboration slide.png') }}" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Collaboration and Networking:</h5>
+            <p>facilitate collaboration between content creators, allowing them to work together on joint projects, share resources, and build connections within the industry. </p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+</div>
+
+
+
+
+<div class="container text-center">
+    <h2 style="color: #dc3545" id="About us">About us</h2>
+
+
+</div>
+
+
+
+
+<div class="container-xl">
+  <div class="row">
+    <div class="col-md-6 card-gap">
+      <div class="card border-danger">
+        <!-- Card content here -->
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="{{ asset('images\MB LOGO.png') }}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Mohammed Barq</h5>
+                <hr>
+                <p class="card-text">Owner, Lead Developer and Designer for the platform <br> Major in Computer Science from An-Najah National University</p>
+                <p class="card-text"><small class="text-muted">From: Nablus-Palestine</small></p>
+              </div>
+            </div>
+        </div>
+      </div>
+  </div>
+    <div class="col-md-6 card-gap">
+      <div class="card border-danger">
+        <!-- Card content here -->
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="{{ asset('images\instructor.jpg') }}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Samer Huwari</h5>
+                <hr>
+                <p class="card-text">CEO of Auxilium Technology and Project instructor <br> Computer Science Instructor at An-Najah National University</p>
+                <p class="card-text"><small class="text-muted">From: Nablus-Palestine</small></p>
+              </div>
+            </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+
+
 
 
 
